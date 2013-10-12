5677b2ca8a6cbf3db3267b0074b468798b78214e
Ryan Harrison <mail@ryanharrison.co.uk>
2013-10-12 17:36:48 +0100

    Added a view model for the main window and set data context in the xaml code

---
+++ RegexDesigner/ViewModels/RegexViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using RegexDesigner.Annotations;
+
+namespace RegexDesigner.ViewModels
+{
+    class RegexViewModel : INotifyPropertyChanged
+    {
+        public RegexViewModel()
+        {
+            InputText = "Some default input text";
+            Global = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string inputText;
+
+        public string InputText
+        {
+            get { return inputText; }
+            set
+            {
+                if (inputText == value) return;
+                inputText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool global;
+
+        public bool Global
+        {
+            get { return global; }
+            set
+            {
+                if (global == value) return;
+                global = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
